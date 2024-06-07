b664e09de426e64c9d63c54fdb2ae3c05b1d13c5
M3xxx404 <160330326+M3xxx404@users.noreply.github.com>
2024-06-07 06:10:43 +0000

    Добавил 8 задание

--- javascript/lab2/lab2.js
+++ javascript/lab2/lab2.js
@@ -76,3 +76,19 @@ function sum(...args){
     for(let i of args) s+=i
     return s
 }
+/**
+* Добавляет свойство "blackSpot" к объекту, используя уникальный символ
+* @param {object} obj объект, к которому нужно добавить свойство
+* @return Исходный объект с добавленным свойством "blackSpot"
+*/
+function addBlackSpot(obj) {
+if (typeof obj !== 'object' || obj === null) {
+throw new Error('obj должен быть объектом');
+}
+
+if (!obj.hasOwnProperty(Symbol.for('blackSpot'))) {
+obj[Symbol.for('blackSpot')] = true;
+}
+
+return obj;
+}
